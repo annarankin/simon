dd2f7bf51975556dc4651af7084ff7f5f0748e5d
Anna Rankin <anna@annarankin.com>
2015-11-07 14:45:45 -0500

    port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var ejs = require('ejs');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
@@ -9,6 +10,6 @@ app.get('/', function(req, res) {
   res.render("index");
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("I'm lisinin");
 })
